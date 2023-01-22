9fb3af3bb9bbdf8fd3544211021b3e9f4dad655e
Helaman99 <arose13@eagles.ewu.edu>
2023-01-22 13:18:32 -0800

    Made ClassName an auto property

--- Logger/BaseLogger.cs
+++ Logger/BaseLogger.cs
@@ -5,9 +5,7 @@
         private string? _FilePath;
         public string FilePath { get => _FilePath!; set => _FilePath = value; }
 
-        private string? _ClassName;
-
-        public string ClassName { get => _ClassName!; set => _ClassName = value; }
+        public string? ClassName { get; set; }
 
         public abstract void Log(LogLevel logLevel, string message);
     }
