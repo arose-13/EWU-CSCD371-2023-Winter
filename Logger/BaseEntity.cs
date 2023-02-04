92ae956e2a439e02d4c16f28714fb8037e1280da
Helaman99 <80353517+arose-13@users.noreply.github.com>
2023-02-04 13:24:04 -0800

    Task 3 - BaseEntity
    `BaseEntity` implicitly implements the interface `IEntity`, but not `Name`. Justification for this is provided.

--- Logger/BaseEntity.cs
+++ Logger/BaseEntity.cs
@@ -1,6 +1,9 @@
 namespace Logger;
 
-abstract class BaseEntity : IEntity{
-    public abstract Guid ID {get; init;}
+// Implicit implementation of `IEntity` allows us to declare `Name` as abstract, thus putting the responsibility of
+// implementation on deriving classes.
+abstract class BaseEntity : IEntity
+{
+    public Guid ID {get; init;}
     public abstract string Name {get; set;}
 }
