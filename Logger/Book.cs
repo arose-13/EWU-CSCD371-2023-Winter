b5cdc1e7c1fae449bd99ff47a335312b2aa31e56
Howard404 <Gh031800#>
2023-02-05 19:04:20 -0800

    Created Book record

---
+++ Logger/Book.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Logger;
+
+public record Book : BaseEntity
+{
+    public override string Name
+    {
+        get
+        {
+            ArgumentNullException.ThrowIfNullOrEmpty(ISBN);
+            ArgumentNullException.ThrowIfNullOrEmpty(BookTitle);
+            return $"{BookTitle}, ISBN: {ISBN}, Author: {Author}";
+        }
+    }
+    public string? BookTitle { get; init; }
+    public FullName Author { get; init; }
+    public string? ISBN { get; init; }
+
+    public Book(Guid id, string firstName, string lastName, string? middleName)
+    {
+        ID = id;
+        Author = new FullName(firstName, lastName, middleName);
+    }
+}
