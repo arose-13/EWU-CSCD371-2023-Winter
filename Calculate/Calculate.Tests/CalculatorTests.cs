6b1f47093af1d68d470f1eb1b7f57c5e2a7e1135
Helaman99 <80353517+arose-13@users.noreply.github.com>
2023-02-23 15:29:23 -0800

    Update CalculatorTests.cs

--- Calculate/Calculate.Tests/CalculatorTests.cs
+++ Calculate/Calculate.Tests/CalculatorTests.cs
@@ -3,8 +3,9 @@ namespace Calculate.Tests;
 [TestClass]
 public class CalculatorTests
 {
+    // ---------- Operations Tests ---------- \\
     [TestMethod]
-    public void Calculate_GivenValidOperations_AllOperationsReturnCorrectResult()
+    public void Add_GivenValidOperations_ReturnCorrectResult()
     {
         // Assemble
         int result = 0;
@@ -17,31 +18,122 @@ public class CalculatorTests
         program.WriteLine(program.calculator.TryCalculate("3 + 3"));
         Assert.AreEqual<int>(result, 6);
 
-        program.WriteLine(program.calculator.TryCalculate("3 - 3"));
+        program.WriteLine(program.calculator.TryCalculate("823 + 100"));
+        Assert.AreEqual<int>(result, 923);
+
+        program.WriteLine(program.calculator.TryCalculate("0 + 0"));
         Assert.AreEqual<int>(result, 0);
 
-        program.WriteLine(program.calculator.TryCalculate("3 * 3"));
-        Assert.AreEqual<int>(result, 9);
-
-        program.WriteLine(program.calculator.TryCalculate("3 / 3"));
-        Assert.AreEqual<int>(result, 1);
+        program.WriteLine(program.calculator.TryCalculate("12 + 0"));
+        Assert.AreEqual<int>(result, 12);
     }
 
     [TestMethod]
-    public void Calculate_GivenOperationWithRemainder_WritesDecimal()
+    public void Subtract_GivenValidOperations_ReturnCorrectResult()
     {
         // Assemble
-        int output = 0;
+        int result = 0;
         Program program = new Program
         {
-            WriteLine = (string input) => { output = int.Parse(input); },
+            WriteLine = (string input) => { result = int.Parse(input); },
+        };
+
+        // Act and Assert
+        program.WriteLine(program.calculator.TryCalculate("3 - 3"));
+        Assert.AreEqual<int>(result, 0);
+
+        program.WriteLine(program.calculator.TryCalculate("326 - 310"));
+        Assert.AreEqual<int>(result, 16);
+
+        program.WriteLine(program.calculator.TryCalculate("0 - 0"));
+        Assert.AreEqual<int>(result, 0);
+
+        program.WriteLine(program.calculator.TryCalculate("12 - 0"));
+        Assert.AreEqual<int>(result, 12);
+    }
+
+    [TestMethod]
+    public void Multiply_GivenValidOperations_ReturnCorrectResult()
+    {
+        // Assemble
+        int result = 0;
+        Program program = new Program
+        {
+            WriteLine = (string input) => { result = int.Parse(input); },
+        };
+
+        // Act and Assert
+        program.WriteLine(program.calculator.TryCalculate("3 * 3"));
+        Assert.AreEqual<int>(result, 9);
+
+        program.WriteLine(program.calculator.TryCalculate("350 * 100"));
+        Assert.AreEqual<int>(result, 35000);
+
+        program.WriteLine(program.calculator.TryCalculate("0 * 0"));
+        Assert.AreEqual<int>(result, 0);
+
+        program.WriteLine(program.calculator.TryCalculate("12 * 0"));
+        Assert.AreEqual<int>(result, 0);
+    }
+
+    [TestMethod]
+    public void Divide_GivenValidOperations_ReturnCorrectResult()
+    {
+        // Assemble
+        int result = 0;
+        Program program = new Program
+        {
+            WriteLine = (string input) => { result = int.Parse(input); },
+        };
+
+        // Act and Assert
+        program.WriteLine(program.calculator.TryCalculate("9 / 3"));
+        Assert.AreEqual<int>(result, 3);
+
+        program.WriteLine(program.calculator.TryCalculate("799 / 100"));
+        Assert.AreEqual<int>(result, 7);
+
+        program.WriteLine(program.calculator.TryCalculate("1 / 1"));
+        Assert.AreEqual<int>(result, 1);
+
+        program.WriteLine(program.calculator.TryCalculate("0 / 12"));
+        Assert.AreEqual<int>(result, 0);
+    }
+
+    // ---------- Error Checking ---------- \\
+
+    [TestMethod]
+    public void Calculate_GivenCharacters_WritesErrorMessage()
+    {
+        // Assemble
+        string output = "Dummy test.";
+        Program program = new Program
+        {
+            WriteLine = (string input) => { output = input; },
         };
 
         // Act
-        program.WriteLine(program.calculator.TryCalculate("5 / 2"));
+        program.WriteLine(program.calculator.TryCalculate("abcd"));
 
         // Assert
-        Assert.AreEqual<int>(output, 2);
+        Assert.IsTrue(output.Contains("Error: "));
+    }
+
+    [TestMethod]
+    public void Calculate_GivenJustNumbers_WritesErrorMessage()
+    {
+        // Assemble
+        string output = "Dummy test.";
+        Program program = new Program
+        {
+            WriteLine = (string input) => { output = input; },
+        };
+
+        // Act
+        program.WriteLine(program.calculator.TryCalculate("1 244"));
+
+        // Assert
+        Assert.IsTrue(output.Contains("Error: "));
     }
 
     [TestMethod]
